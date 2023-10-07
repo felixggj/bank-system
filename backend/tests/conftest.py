a18dabcef926ddcc2e57eed5d3f97534a88d14e6
felixggj <me@felix-gomez.com>
2023-10-08 01:24:05 +0200

    fixing frontend error on azure attempt 8

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -2,20 +2,17 @@ import pytest
 from iebank_api.models import Account
 from iebank_api import db, app
 
-@pytest.fixture(scope='module')
-def testing_client():
-    # Push an application context to the context stack
-    app_context = app.app_context()
-    app_context.push()
 
-    db.create_all()
-    account = Account('Test Account', '€')
-    db.session.add(account)
-    db.session.commit()
+@pytest.fixture
+def testing_client(scope="module"):
+    with app.app_context():
+        db.create_all()
+        account = Account("Test Account", "A Country", "€")
+        db.session.add(account)
+        db.session.commit()
 
     with app.test_client() as testing_client:
         yield testing_client
 
-    db.drop_all()
-    # Pop the application context from the context stack
-    app_context.pop()
+    with app.app_context():
+        db.drop_all()
--- backend/tests/functional/test_routes.py
+++ backend/tests/functional/test_routes.py
@@ -1,14 +1,16 @@
 from iebank_api import app
 import pytest
 
+
 def test_get_accounts(testing_client):
     """
     GIVEN a Flask application
     WHEN the '/accounts' page is requested (GET)
     THEN check the response is valid
     """
-    response = testing_client.get('/accounts')
+    response = testing_client.get("/accounts")
     assert response.status_code == 200
+
 
 def test_dummy_wrong_path():
     """
@@ -17,8 +19,9 @@ def test_dummy_wrong_path():
     THEN check the response is valid
     """
     with app.test_client() as client:
-        response = client.get('/wrong_path')
+        response = client.get("/wrong_path")
         assert response.status_code == 404
+
 
 def test_create_account(testing_client):
     """
@@ -26,7 +29,9 @@ def test_create_account(testing_client):
     WHEN the '/accounts' page is posted to (POST)
     THEN check the response is valid
     """
-    response = testing_client.post('/accounts', json={'name': 'John Doe', 'currency': '€'})
+    response = testing_client.post(
+        "/accounts", json={"name": "John Doe", "country": "Spain", "currency": "€"}
+    )
     assert response.status_code == 200
 
 
