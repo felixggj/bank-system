c8ec1e08fbe6458d89cc1e4010ed163bb73d1fe5
Felix Gomez-Guillamon <104097257+felixggj@users.noreply.github.com>
2023-10-08 21:07:45 +0200

    Added home page
    * Added Home Page
    
    * HomePage added

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,27 +1,33 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Skull from '../components/Skull.vue'
-import AppAccounts from '../components/AppAccounts.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Skull from "../components/Skull.vue";
+import AppAccounts from "../components/AppAccounts.vue";
+import HomePage from "../components/HomePage.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/skull',
-    name: 'Skull',
-    component: Skull
+    path: "/skull",
+    name: "Skull",
+    component: Skull,
   },
   {
-    path: '/accounts',
-    name: 'AppAccounts',
-    component: AppAccounts
-  }
-]
+    path: "/accounts",
+    name: "AppAccounts",
+    component: AppAccounts,
+  },
+  {
+    path: "/",
+    name: "HomePage",
+    component: HomePage,
+  },
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;
