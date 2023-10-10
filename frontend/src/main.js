c766fffd5a7e9a29dcbf788f8231ce9265119b83
felixggj <me@felix-gomez.com>
2023-10-10 11:22:55 +0200

    update: attempt 3 deployment

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css' // Install Bootstrap first
 import BootstrapVue from "bootstrap-vue";
 
 
