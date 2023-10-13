6086238bdd02db10b019825bfba53be14fabe3a6
felixggj <me@felix-gomez.com>
2023-10-13 15:00:08 +0200

    Added 2 new functional and unit tests

--- backend/tests/functional/test_routes.py
+++ backend/tests/functional/test_routes.py
@@ -35,3 +35,36 @@ def test_create_account(testing_client):
     assert response.status_code == 200
 
 
+def test_update_account(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/accounts/<id>' page is requested (PUT)
+    THEN check the response is valid and the account data is updated
+    """
+    # You might want to create an account first to ensure the ID you will use exists
+    testing_client.post("/accounts", json={"name": "Test", "country": "Testland", "currency": "$"})
+    
+    # Replace <id> with the ID of the account you want to update
+    response = testing_client.put("/accounts/1", json={"name": "Updated Test"})
+    assert response.status_code == 200    
+
+
+def test_delete_account(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/accounts/<id>' page is requested (DELETE)
+    THEN check the response is valid and the account is deleted
+    """
+    # Create an account first to ensure the ID you will use exists
+    response_post = testing_client.post("/accounts", json={"name": "Test", "country": "Testland", "currency": "$"})
+    account_id = response_post.json['id']  # Assuming the post response contains the created account ID
+    
+    # Replace <id> with the ID of the account you want to delete
+    response = testing_client.delete(f"/accounts/{account_id}")
+    assert response.status_code == 200
+    
+    # Additional check: Ensure the account is no longer available
+    response_get = testing_client.get(f"/accounts/{account_id}")
+    assert response_get.status_code == 404
+
+
